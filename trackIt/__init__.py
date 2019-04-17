e68f7debdad954dabfddd5dcc450206dd2bff453
Jacob Hanson <jacobhanson@Jacobs-MacBook-Pro.local>
2019-04-17 00:01:55 -0700

    created login system with flask-login

--- trackIt/__init__.py
+++ trackIt/__init__.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
@@ -9,5 +10,7 @@ app.config['SECRET_KEY'] = '31f214ac7307802de7160100ec7a549b'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt()
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 from trackIt import routes
--- trackIt/routes.py
+++ trackIt/routes.py
@@ -1,21 +1,25 @@
 from flask import render_template, flash, redirect, url_for
+from flask_login import login_user, current_user, logout_user, login_required
 from trackIt.models import User, Item, Entry
 from trackIt import app, db, bcrypt
 from trackIt.forms import NewEntryForm, LoginForm, RegistrationForm
 
 
-@app.route("/", methods=['GET' ,'POST'])
+@app.route("/home", methods=['GET' ,'POST'])
+@login_required
 def home():
 	return render_template('index.html', saleForm=NewEntryForm(), purchForm=NewEntryForm())
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def login():
-	form = LoginForm();
-	if form.validate_on_submit(): 
-		username = form.username.data
-		password = form.password.data
-		users = User.query.all()
-		return redirect(url_for('home'))
+	form = LoginForm()
+	if form.validate_on_submit():
+		user = User.query.filter_by(username=form.username.data).first()
+		if user and bcrypt.check_password_hash(user.password, form.password.data):
+			login_user(user, remember=form.remember.data)
+			return redirect(url_for('home'))
+		else:
+			flash('Login Unsuccessful', 'failure')
 	return render_template('login.html', form=form)
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -30,3 +34,8 @@ def register():
 		return redirect(url_for('login'))
 	print(form.errors)
 	return render_template('register.html', form=form)
+
+@app.route('/logout')
+def logout():
+	logout_user()
+	return redirect(url_for('home'))
